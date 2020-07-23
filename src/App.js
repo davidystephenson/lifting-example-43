1415467b41cc2f748ac9c0cf4f212da81e1e9e91
David Y. Stephenson <david@davidystephenson.com>
2020-07-23 11:37:23 +0200

    Toggle data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Row ({ teacher, onClick }) {
+  // const { teacher, onClick } = props
+
+  const { name, available } = teacher
+
+  const message = available
+    ? 'Yes!'
+    : 'No.'
+
+  return <tr>
+    <td>{name}</td>
+    <td>{message}</td>
+    <td>
+      <button
+        onClick={() => onClick(name)}
+      >
+        toggle
+      </button>
+    </td>
+  </tr>
+}
+
+function App () {
+  const [teachers, setTeachers] = useState([
+    { name: 'rein', available: true },
+    { name: 'matias', available: false },
+    { name: 'david', available: true }
+  ])
+
+  function onClick (teacherName) {
+    // make a new array with updated data
+    // - property: available
+    // - teacher: teacher?
+    const newTeachers = teachers.map(teacher => {
+      const rightTeacher = teacher.name === teacherName
+
+      if (rightTeacher) {
+        const newAvailable = !teacher.available
+
+        const copy = {
+          ...teacher,
+          available: newAvailable
+        }
+
+        return copy
+      }
+
+      return teacher
+    })
+
+    // replace the array in the state
+    setTeachers(newTeachers)
+  }
+
+  const rows = teachers.map(teacher => (
+    <Row
+      teacher={teacher}
+      key={teacher}
+      onClick={onClick}
+    />
+  ))
+
+  const total = teachers
+    .filter(teacher => teacher.available)
+    .length
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Available</th>
+          <th>Toggle</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+
+      total: {total}
+
+    </table>
   );
 }
 
